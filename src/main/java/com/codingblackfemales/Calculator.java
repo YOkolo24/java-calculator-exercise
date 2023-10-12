cb116be2f1f66c5ef85105631e193a4df9748a9a
Emem Attah <ememattah7@hotmail.co.uk>
2023-10-12 21:19:42 +0100

    12/10- Exercise submission to create tests for multiplication & division methods.

--- src/main/java/com/codingblackfemales/Calculator.java
+++ src/main/java/com/codingblackfemales/Calculator.java
@@ -1,19 +1,19 @@
 package com.codingblackfemales;
 
 public class Calculator {
-    public static Integer add(final int firstInput, final int secondInput) {
+    public static Float add(final float firstInput, final float secondInput) {
         return firstInput + secondInput;
     }
 
-    public static Integer subtract(final int firstInput, final int secondInput) {
+    public static Float subtract(final float firstInput, final float secondInput) {
         return firstInput - secondInput;
     }
 
-    public static Integer multiply(final int firstInput, final int secondInput) {
+    public static Float multiply(final float firstInput, final float secondInput) {
         return firstInput * secondInput;
     }
 
-    public static Integer divide(final int firstInput, final int secondInput) {
-        return firstInput / secondInput;
+    public static Float divide(final float firstInput, float secondInput) { return firstInput / secondInput;
+
     }
 }
--- src/test/java/com/codingblackfemales/CalculatorTests.java
+++ src/test/java/com/codingblackfemales/CalculatorTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName(value = "The Calculator should")
 public class CalculatorTests {
@@ -11,7 +12,7 @@ public class CalculatorTests {
     @Test
     @DisplayName("add two numbers")
     public void testAddition() {
-        final Integer sum = Calculator.add(2, 4);
+        final Float sum = Calculator.add(2, 4);
 
         assertEquals(6, sum);
     }
@@ -19,8 +20,38 @@ public class CalculatorTests {
     @Test
     @DisplayName("subtract two numbers")
     public void testSubtraction() {
-        final Integer difference = Calculator.subtract(2, 4);
+        final Float difference = Calculator.subtract(2, 4);
 
         assertEquals(-2, difference);
     }
+
+    @Test
+    @DisplayName("multiply two numbers")
+    public void testMultiplication() {
+        final Float product = Calculator.multiply(2, 4);
+
+        assertEquals(8, product);
+    }
+
+    @Test
+    @DisplayName("divide two numbers")
+    public void testDivision() {
+        final float remainder = Calculator.divide(2, 4);
+
+        assertEquals(0.5, remainder);
+    }
+
+    @Test
+    @DisplayName("divide by Zero throws exception")
+    public  void divideZero(){
+        // final Integer result = Calculator.divide(5,0);
+
+        assertThrows(ArithmeticException.class, () ->{ Calculator.divide(5,0);
+        });
+
+    }
+
+
+
+
 }
